d993b06d5c6a6eb2d60c645826032b807927276e
Joseph Mariga <jsphmariga@gmail.com>
2022-03-14 14:23:15 -0700

    Add lib and time header files

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -9,6 +10,10 @@
 
 int main(void)
 {
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
