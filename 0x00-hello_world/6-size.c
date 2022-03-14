938a02f414672b4b415cfe081455aaad70213e17
Joseph Mariga <jsphmariga@gmail.com>
2022-03-14 03:07:52 -0700

    Add colon

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,20 +3,21 @@
 /**
  * main - entry point
  *
- * Return: 0 (Success)
+ * Description: prints the size in byte that a data type occupies in the computer memory.
+ * Return: 0.
  */
 int main(void)
 {
 	char c;
 	int i;
-	long int li;
+	long li;
 	long long ll;
 	float f;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int %ld byte(s)\n", sizeof(ll));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(ll));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
