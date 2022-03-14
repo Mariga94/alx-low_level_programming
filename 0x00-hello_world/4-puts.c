6c3cf696f6d9254f5c0347afe662df25424df72a
Joseph Mariga <jsphmariga@gmail.com>
2022-03-13 17:01:20 -0700

    Add description

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 
-/*
- * main - prints "Programming is like building a multilingual puzzle"
+/**
+ * main - prints string in puts
+ *
+ * Description: This is a longer description.
+ * The program prints Programming is like building a multilingual puzzle
+ * Return: 0
  */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
