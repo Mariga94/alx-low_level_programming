8ad1065cca097a21a42c336ca3eeb58938a7b253
Joseph <jsphmariga@gmail.com>
2022-03-15 21:40:03 +0000

    Add print_alphabet prototype [Task 1]

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,4 +12,23 @@ char *display(void)
 {
 	return ("_putchar\n");
 }
+
+/**
+ * print_alphabet - print lowercase alphabets
+ *
+ * Return: character
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		_putchar(i);
+		_putchar('\n');
+		return;
+}
+
+
 #endif /*MAIN_H*/
