7a0e62ea5fa2faf28c9dab9334a8f1501558538e
Joseph <jsphmariga@gmail.com>
2022-03-15 20:57:24 +0000

    Add include guards

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,15 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
  * display - prints _putchar to stdout
+ *@void - no parameter
  *
+ *Return: "putchar" (Success)
  */
 
-char* display(void)
+char *display(void)
 {
 	return ("_putchar\n");
 }
+#endif /*MAIN_H*/
