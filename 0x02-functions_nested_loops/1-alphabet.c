9f37986106f8e7ed343b0f170e60dd3c1fc028a9
Joseph <jsphmariga@gmail.com>
2022-03-15 21:38:56 +0000

    Write a program that prints the alphabet, in lowercase [Task 1]

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* main - entry point
+*
+* Return: 0 Always (Success)
+*/
+
+/*function declaration */
+void print_alphabet(void);
+
+int main(void)
+{
+	/* function call */
+	print_alphabet();
+	return (0);
+}
+
