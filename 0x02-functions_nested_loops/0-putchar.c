f27fe50a11b17fd9434cbe534f105e82a705357b
Joseph <jsphmariga@gmail.com>
2022-03-15 19:56:04 +0000

    Write a program that prints _putchar [Task 0._putchar]

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+
+/**
+ * display - prints _putchar to stdout
+ *
+ * Return: 0 Always (Success)
+ *
+ */
+
+/* function priting _putchar */
+
+int display(void)
+{
+	return ("_putchar\n");
+	return (0);
+}
+
