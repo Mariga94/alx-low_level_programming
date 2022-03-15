85bfe1e86220545603a56a052b833447a67803dd
Joseph <jsphmariga@gmail.com>
2022-03-15 23:03:04 +0000

    Make changes

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,36 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * display - prints _putchar to stdout
- *@void - no parameter
- *
- *Return: "putchar" (Success)
- */
-
-char *display(void)
-{
-	return ("_putchar\n");
-}
-
-/**
- * print_alphabet - print lowercase alphabets
- *
- * Return: character
- */
-
-void print_alphabet(void)
-{
-	char i;
-
-	for (i = 'a'; i < 'z'; i++)
-	{
-		_putchar(i);
-	}
-		_putchar('\n');
-		return;
-		
-}
+int _putchar(char);
+void print_alphabet(void);
 
 
 #endif /*MAIN_H*/
