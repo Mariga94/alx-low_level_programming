d5bf3b2ea5084c44ea203d41fb712fd9a4990575
Joseph <jsphmariga@gmail.com>
2022-03-16 02:42:50 +0000

    Update file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,15 +9,14 @@
 
 int print_last_digit(int c)
 {
-	if (c > 0 || c == 0)
+	if (c > 0)
 	{
 	_putchar (c % 10 + '0');
 	return (c % 10);
 	}
 	else
 	{
-	c = c * -1;
-	_putchar (c % 10 + '0');
+	_putchar (c % 10 * -1);
 	return (c % 10);
 	}
 }
