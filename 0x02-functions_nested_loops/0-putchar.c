02aaa00cca2f20399162b1279869e144d8b482f9
Joseph <jsphmariga@gmail.com>
2022-03-16 20:04:57 +0000

    Write a program that prints _putchar followed by a new line

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,8 +5,15 @@
  * Return: on success 0.
  */
 
-char *display(void)
+int display(void)
 {
-	_putchar("_putchar");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 }
