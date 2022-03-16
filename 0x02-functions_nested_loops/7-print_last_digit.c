bae1e68287a2989f4f5e8c39ae080b23fc2c1805
Joseph <jsphmariga@gmail.com>
2022-03-16 01:32:49 +0000

    Correct statements

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,15 @@
 int print_last_digit(int n)
 {
 	if (n > 0)
+	{
 		_putchar(n % 10 + '0');
 		return (n % 10);
+	}
 	else
+	{
 		n = n * -1;
 		_putchar (n % 10 + '0');
-		return (c % 10);	
+		return (c % 10);
+	}
 }
 
