f7d609e60d27fa585d001dc0b5c722da228d6c5d
Joseph <jsphmariga@gmail.com>
2022-03-16 11:26:59 +0000

    Make changes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,7 +2,7 @@
 
 /**
  * print_last_digit - returns last digit
- * @n:The number to be checked
+ * @c:The number to be checked
  *
  * Return: 0 Always
  */
@@ -11,12 +11,12 @@ int print_last_digit(int c)
 {
 	if (c >= 0)
 	{
-	_putchar (c % 10 + '0');
-	return (c % 10);
+		_putchar (c % 10 + '0');
+		return (c % 10);
 	}
-	else
+	else if (c <= 0)
 	{
-	_putchar (c % 10 * -1);
-	return (c % 10);
+		_putchar((c % 10 * -1) + '0');
+		return (c % 10);
 	}
 }
