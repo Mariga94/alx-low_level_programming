e339fbac54b35500f805ecea81fbea2713736975
Joseph Mariga <jsphmariga@gmail.com>
2022-03-17 13:37:30 +0000

    Replace isupper library with for loop

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,21 +3,25 @@
 /**
  * _isupper - Program that checks for uppercase character
  * isupper - man library checks if the passed character is uppercase.
- * @n: Character to be checked.
+ * @c: Character to be checked.
  *
  * Return: 1 if c is uppercase. 0 otherwise
  *
  */
-int isupper(int n);
+
 int _isupper(int c)
 {
-	if (isupper(c))
+	char i;
+
+	for (i = 'A'; i <= 'Z'; i++)
 	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+		if (c == i)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
 }
 
