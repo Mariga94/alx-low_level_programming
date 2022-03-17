df549af5e0c1b98f9583dbd82f90744a07fad90e
Joseph Mariga <jsphmariga@gmail.com>
2022-03-17 14:05:21 +0000

    Add a function that prints the numbers 0 to 9 but not 2 and 4 [Taks 4]

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers from 0 to 9 but not 2 and 4.
+ *
+ * Return: 0 Always
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar(i);
+		}
+
+		i++;
+	}
+	_putchar('\n');
+}
