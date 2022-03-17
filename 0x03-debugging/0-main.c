aac3ecf5eabe67f0ba0854a28255238b646a5715
Joseph Mariga <jsphmariga@gmail.com>
2022-03-17 23:43:55 +0000

    Multiple mains [Task 0]

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - tests if number is positive or negative
+ * @i: number to be checked
+ * 
+ * Return: 0 
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0 )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
