97130d0f97dc24c01eabcef4770d73863f749fde
Joseph Mariga <jsphmariga@gmail.com>
2022-03-18 23:12:59 +0000

    Make file

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,22 +8,14 @@
 
 void more_numbers(void)
 {
-	int i = 0;
-	int j = 0;
+	int i, j;
 
-	while (j <= 9)
+	for (i = 0; i <= 9; i++)
 	{
-		while (i <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-			if (i > 9)
-			{
-				_putchar(i / 10 + '0');
-			}
-			_putchar(i % 10 + '0');
-			i++;
+			_putchar(j + '0');
 		}
-		_putchar('\n');
-		j++;
-		i = 0;
+	_putchar('\n');
 	}
 }
