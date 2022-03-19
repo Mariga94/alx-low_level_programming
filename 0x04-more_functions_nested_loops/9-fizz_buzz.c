6c2f861f78b76bf6c4cfdaafdad0325d0a7d7e57
Joseph Mariga <jsphmariga@gmail.com>
2022-03-19 00:24:14 +0000

    Make correction

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
 /**
- * fizz_buzz - functions that check a number if its a fizz or buzz
- * @num: number to be checked
+ * main - functions that check a number if its a fizz or buzz
  *
  * Return: 0 Always
  */
-
 int main(void)
 {
 	int i = 1;
@@ -14,9 +12,7 @@ int main(void)
 	while (i <= 100)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
-		{
 			printf("FizzBuzz ");
-		}
 		else if (i % 5 == 0)
 		{
 			if (i == 100)
@@ -25,18 +21,13 @@ int main(void)
 				printf("\n");
 			}
 			else
-			{
 				printf("Buzz ");
-			}
-			else if (i % 3 == 0)
-			{
-				printf("Fizz ");
-			}
-			else
-			{
-				printf("%d ", i);
-			}
-			i++;
 		}
-		return (0);
+		else if (i % 3 == 0)
+			printf("Fizz ");
+		else
+			printf("%d ", i);
+	i++;
 	}
+return (0);
+}
