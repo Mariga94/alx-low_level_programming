15807757c07eca27c25750d9e5a0fd407d63ca66
Joseph Mariga <jsphmariga@gmail.com>
2022-03-22 13:29:13 +0000

    Add description to function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * _strcpy(char *dest, char *src) - copies the string pointed to by src
+ * *_strcpy(char *dest, char *src) - copies the string pointed to by src
  * @dest: 1st parameter
  * @src: 2nd parameter
  *
- * Return : 0 Always
+ * Return: 0 Always
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0; i < src[i] != '\0'; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[j];
 	}
