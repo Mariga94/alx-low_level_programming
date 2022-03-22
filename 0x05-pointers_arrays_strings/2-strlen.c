fc9c0bcd58e57bb030003cc2de595b4cce26815f
Joseph Mariga <jsphmariga@gmail.com>
2022-03-22 14:26:09 +0000

    Add length variable to store output

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,17 +1,21 @@
 #include "main.h"
 
 /**
- * _strlen - calculate length of str
- * @s: string to be counted
+ * _strlen - calculate length of s.
+ * @s: string to be counted.
  *
- * Return: length of string
+ * Return: length of string.
  */
 
 int _strlen(char *s)
 {
 	int i;
+	int length = 0;
 
-	for (i = 0; s[i] != '\0'; i++);
-	return (i);
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		length = i;
+	}
+	return (length);
 }
 
