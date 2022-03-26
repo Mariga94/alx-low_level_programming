53dde707f6130d14d0d9563bac8ebef40892c75c
Joseph Mariga <jsphmariga@gmail.com>
2022-03-26 02:19:01 +0000

    Change iteration to while loop

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,37 +10,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j,len_s1, len_s2;
-
-	/*find length of both strings */
-
-	for (i = 0; s1[i] != '\0'; i++)
+	while (*s1 == *s2)
 	{
-		len_s1 = i;
-	}
-	for (j = 0; s2[j] != '\0'; j++)
-	{
-		len_s2 = j;
-	}
-
-	if (len_s1 != len_s2)
-	{
-		return (*s1-*s2);
-	}
-	else
-	{
-		for (i = 0; i < len_s1; i++)
+		if (*s1 == '\0')
 		{
-			if (s1[i] != s2[i])
-			{
-				return (1);
-			}
-			else
-			{
-				return (0);
-			}
+			return (0);
 		}
-	
+		s1++;
+		s2++;
 	}
-	return (*s1-*s2);
+	return (*s1 - *s2);
 }
