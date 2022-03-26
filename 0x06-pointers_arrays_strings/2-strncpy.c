df998119d24a97648eadda1d7d74fc83c6359bb7
Joseph Mariga <jsphmariga@gmail.com>
2022-03-26 02:25:28 +0000

    Write a function that copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -8,18 +8,23 @@
  *
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i]; i++)
+	i = 0;
+
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
+		i++;
 	}
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
 		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
