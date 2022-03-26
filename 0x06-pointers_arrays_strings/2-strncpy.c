ac061830c7cd635044d345fadc3fe9ed9fbff614
Joseph Mariga <jsphmariga@gmail.com>
2022-03-26 02:23:24 +0000

    Check for n

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,7 +14,13 @@ char *_strncpy(char *dest, char *src, int n)
 
 	for (i = 0; src[i]; i++)
 	{
-		dest[n] = src[i];
+		dest[i] = src[i];
 	}
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
 	return (dest);
 }
