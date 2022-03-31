6803e1e1b2aa7f4fb783549b37059344bf2b24b4
Joseph Mariga <jsphmariga@gmail.com>
2022-03-31 21:13:55 +0000

    Correct betty styling

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - calculates the power of x raised to power of y
  * @x: value to be calculate
  * @y: power
- * Returns: power of x raised to power of y
+ * Return: power of x raised to power of y
  */
 
 int _pow_recursion(int x, int y)
@@ -17,11 +17,10 @@ int _pow_recursion(int x, int y)
 	{
 		return (1);
 	}
-
 	else if (x == 0)
-	 {
-		 return (0);
-	 }
+	{
+		return (0);
+	}
 	else
 	{
 		return (x * _pow_recursion(x, y - 1));
