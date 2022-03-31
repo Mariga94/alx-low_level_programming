d34c309dd91783d4435e015a25b2e3915fb93336
Joseph Mariga <jsphmariga@gmail.com>
2022-03-31 21:22:12 +0000

    Write a function that compares two strings[Task 8]

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * wildcmp - compares 2 strings
+ * @s1: string 1
+ * @s2: string 2
+ *
+ * Return: 1 if the string are identical Otherwise 0.
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (1);
+	}
+
+	return (0);
+}
