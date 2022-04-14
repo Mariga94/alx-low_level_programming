9ed543ac139e3aed6ac1b07b3be2e4160da1b141
Joseph Mariga <jsphmariga@gmail.com>
2022-04-14 02:37:12 +0000

    0. What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#inlclude "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name of object
+ * @f: pointer to function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+	{
+		f(name);
+	}
+}
