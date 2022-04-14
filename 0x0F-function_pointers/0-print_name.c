044ae90ee0dd9d92fdd0666f250e6b363a5f4d7a
Joseph Mariga <jsphmariga@gmail.com>
2022-04-14 02:42:28 +0000

    Correct file

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
-#inlclude "function_pointers.h"
+#include <stddef.h>
+#include "function_pointers.h"
 /**
  * print_name - prints a name
  * @name: name of object
@@ -9,6 +10,6 @@ void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 	{
-		f(name);
+		(*f)(name);
 	}
 }
