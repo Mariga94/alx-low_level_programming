68713e3a33369eac57361a3b409e4a3916032f55
Joseph Mariga <jsphmariga@gmail.com>
2022-04-15 00:48:17 +0000

    vagrant@ubuntu-focal:~/alx-low_level_programming/0x10-variadic_functions

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - function that prints strings
+ * @separator: separate strings
+ * @n: number of strings passed
+ *
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+	char *res;
+
+	va_start(ap, n);
+	for (i = 0; i < n; ++i)
+	{
+		res = va_arg(ap, char*);
+		if (res != NULL)
+		{
+			printf("%s", res);
+		}
+		else
+		{
+			printf("(nill)");
+		}
+
+		if (separator && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(ap);
+}
