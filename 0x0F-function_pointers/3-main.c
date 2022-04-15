2a4f055f3d6817b7d754d91f2f23cb885f31a64a
Joseph Mariga <jsphmariga@gmail.com>
2022-04-15 23:45:59 +0000

    Update file

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,12 +7,25 @@
  * Return: 0 Always
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+	int a, b, c;
+	int (*p)(int, int);
 
 	if (argc == 4)
 	{
-		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+		a = atoi(argv[1]);
+		b = atoi(argv[3]);
+
+		p = get_op_func(argv[2]);
+		if (p == NULL)
+		{
+			printf("Error\n");
+			exit(99);
+		}
+		c = (*p)(a, b);
+		
+		printf("%d\n", c);
 		return (0);
 	}
 	printf("Error\n");
