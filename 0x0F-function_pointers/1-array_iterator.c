288a95acd839f46fee2fbce405fb8b9ce1fdc74e
Joseph Mariga <jsphmariga@gmail.com>
2022-04-16 00:16:47 +0000

    Add condition

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i = 0;
 
-	while (i < size)
-	{
-		(*action)(array[i]);
-		i++;
-	}
+	if (array && size && action)
+		while (i < size)
+		{
+			(*action)(array[i]);
+			i++;
+		}
 }
