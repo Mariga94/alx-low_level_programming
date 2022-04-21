fad6427dcd89634db6987c26f0c7443d2c2c5355
Joseph Mariga <jsphmariga@gmail.com>
2022-04-21 14:15:30 +0000

    Add Functions that are executed before main.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-#define start main
-void start()
+/**
+ * myStartupFun - prints code before main.c
+ * Return: nothing
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+void myStartupFun(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
