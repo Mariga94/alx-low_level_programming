7563e68277bc937148a32585aa18231a98a45840
Joseph Mariga <jsphmariga@gmail.com>
2022-04-21 13:27:02 +0000

    2. Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#inlclude "lists.h"
+int _strlen(const char *s);
+/**
+ * add_node - adds a new node at the beginning of a list
+ * @head: Beginning of a list
+ * @str: string to add
+ * Return: address of new element, or NULL if it failed
+ */
+list *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int length = _strlen(str);
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = length;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+ * _strlen - calculates the length of the string
+ * @s: string
+ * Return: length of the string
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
