48ada337a0a200df42e24fadba4dc674a9bd3af8
Joseph Mariga <jsphmariga@gmail.com>
2022-04-22 00:24:16 +0000

    Add files

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <stdlib.h>
 /**
  * main - checks code
  * @argc: count of inputs
@@ -7,29 +6,28 @@
  *
  * Return: 0 Always
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int a, b, c;
 	int (*p)(int, int);
 
-	if (argc == 4)
+	if (argc != 4)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[3]);
-
-		p = get_op_func(argv[2]);
-		if (p == NULL)
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		c = (*p)(a, b);
-
-		printf("%d\n", c);
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
-	printf("Error\n");
-	return (98);
-}
 
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	p = get_op_func(argv[2]);
+	if (p == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = (*p)(a, b);
+
+	printf("%d\n", c);
+	return (0);
+}
