e74eff74634b20ead2dd2142e69e8a78565095d2
Joseph Mariga <jsphmariga@gmail.com>
2022-05-08 00:26:48 +0000

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,16 +20,15 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		fd = open(filename, O_WRONLY | O_CREATE, 0600);
+		fd = open(filename, O_WRONLY | O_CREAT, 0600);
 		if (fd == -1)
 			return (-1);
 		return (1);
 	}
-
-	fd = open(filename, O_WRONLY | O_CREATE | O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	return (-1);
+
 	for (len = 0; text_content[len]; len++)
 	{}
 	wr = write(fd, text_content, len);
