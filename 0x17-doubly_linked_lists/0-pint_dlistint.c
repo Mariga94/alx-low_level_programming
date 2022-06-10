410b5728e78b5246aca44a81d33ee8371fc1896e
Joseph Mariga <jsphmariga@gmail.com>
2022-06-10 22:22:45 +0000

    Print list [Task 0]

---
+++ 0x17-doubly_linked_lists/0-pint_dlistint.c
@@ -0,0 +1,19 @@
+include "lists.h"
+/**
+ * print_dlistint - prints all the elements of a linked list
+ * @h: pointer reference to the head node
+ * Return: prints all the elements, else return 0;
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
