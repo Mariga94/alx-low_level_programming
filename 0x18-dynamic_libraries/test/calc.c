28be612e17afc79474dd91f00686a3b7da946500
Joseph Mariga <jsphmariga@gmail.com>
2022-06-13 21:28:39 +0000

    Add test files

---
+++ 0x18-dynamic_libraries/test/calc.c
@@ -0,0 +1,56 @@
+#include "../main.h"
+
+/**
+ * add - adds two integers
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtract two integers
+ * @a: integer
+ * @b: integer
+ * Return: value of sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ * Return: value of mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div two integers
+ * @a: integer
+ * @b: integer
+ * Return: value of div
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - returns remainder of division
+ * @a: integer
+ * @b: integer
+ * Return: value of mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
