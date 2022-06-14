a73d09c6e7784ccb8c531325664d8f8ed2d481dd
Joseph Mariga <jsphmariga@gmail.com>
2022-06-14 12:37:23 +0000

    Add betty styling

--- 0x19-stacks_queues/monty.h
+++ 0x19-stacks_queues/monty.h
@@ -25,9 +25,9 @@ typedef struct stack_s
  * Description: opcode and its function
  * for stack, queues, LIFO, FIFO
  */
-typedef struct instruction_s 
+typedef struct instruction_s
 {
 	char *opcode;
-	void(*f)(stack_t **stack, unsigned int line_number);
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 #endif
