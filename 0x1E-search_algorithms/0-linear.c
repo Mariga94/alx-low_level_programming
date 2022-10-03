61fefc06f1ee3af9c3963ea13f8153774a3a394b
joseph <jsphmariga@gmail.com>
2022-10-03 22:35:23 +0000

    Add linear search algo

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value on an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to look up in the array
+ *
+ * Return: first index where value is located else if value is not
+ * present or if array is NULL return -1
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (!array || !size)
+		return (-1);
+	for (i = 0; i <= size; i++)
+		if (value == array[i])
+			return (i);
+	return (-1);
+}
