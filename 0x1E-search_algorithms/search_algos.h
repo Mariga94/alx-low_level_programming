f6635836b6518f4de6abd4c502c64ed798dcb2f8
joseph <jsphmariga@gmail.com>
2022-10-03 22:34:59 +0000

    Add header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _SEARCH_ALGOS_H
+#define _SEARCH_ALGOS_H
+
+/*standard libraries*/
+#include <stdio.h>
+#include <stdlib.h>
+
+/*prototypes*/
+int linear_search(int *array, size_t size, int value);
+#endif
