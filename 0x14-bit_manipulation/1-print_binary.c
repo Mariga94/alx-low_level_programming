37fa11415ce09850c1cbe285e4567d983f3a465f
joseph <jsphmariga@gmail.com>
2022-12-15 14:29:24 +0000

    Fix betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,7 +9,7 @@ void print_binary(unsigned long int n)
 {
 	int i;
 
-	for (i = 1 << 31; i > 0; i >>= 1)
+	for (i = 1 << 31; i > 0; i = i / 2)
 	{
-		printf("%u", !!(n & i));
+		(n & i) ? printf("1") : printf("0");
 	}
